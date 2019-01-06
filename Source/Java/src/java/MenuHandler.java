70ac696e5d5bf64d42d09e53fe4897fa41e68ebe
EZSPECIAL <ftlopes93@gmail.com>
2019-01-06 02:56:04 +0000

    Java: fix linux clearscreen

--- Source/Java/src/java/MenuHandler.java
+++ Source/Java/src/java/MenuHandler.java
@@ -620,7 +620,7 @@ public class MenuHandler {
 			if (System.getProperty("os.name").contains("Windows"))
 				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 			else
-				Runtime.getRuntime().exec("clear");
+				System.out.print("\033[H\033[2J");
 		} catch (IOException | InterruptedException ex) {
 			System.out.println("Error clearing console!");
 		}
